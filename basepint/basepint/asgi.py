2b8df1f905a768ff1f172da5d53f9c4cb76fb61e
joseline25 <73165802+joseline25@users.noreply.github.com>
2021-01-06 11:13:16 +0100

    Add files via upload

---
+++ basepint/basepint/asgi.py
@@ -0,0 +1,24 @@
+"""
+ASGI config for basepint project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basepint.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+})
+
+
+
+#application = get_asgi_application()
